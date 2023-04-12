b99130288cc725e9ceba4a34d9d383722a9889bc
Romulo Rosa <romulo.frontend@gmail.com>
2023-04-12 19:47:48 -0300

    js canvas programa 2

--- js/canvas_programa_2.js
+++ js/canvas_programa_2.js
@@ -2,19 +2,19 @@ var tela = document.querySelector('.tela');
 
 var pincel = tela.getContext('2d');
 
-function createSquare(color,  colorStroke, positionX, positionY, sizeX, sizeY){
+function createSquare(color, positionX, positionY, sizeX, sizeY, colorStroke){
   pincel.fillStyle = color;
   pincel.fillRect(positionX, positionY,sizeX,sizeY);
   pincel.fillStroke = colorStroke;
   pincel.strokeRect(positionX, positionY,sizeX,sizeY);
 }
 
-createSquare('red', 'black', 0, 0, 100, 100);
-createSquare('green', 'black', 100, 0, 100, 100);
-createSquare('blue', 'black', 200, 0, 100, 100)
-createSquare('yellow', 'black', 300, 0, 100, 100)
+createSquare('red',0,0,100,100,'black');
+createSquare('green',100,0,100,100,'black');
+createSquare('blue',200,0,100,100,'black')
+createSquare('yellow',300,0,100,100,'black')
 
-createSquare('yellow', 'black', 0, 100, 100, 100)
-createSquare('blue', 'black', 100, 100, 100, 100)
-createSquare('green', 'black', 200, 100, 100, 100);
-createSquare('red', 'black', 300, 100, 100, 100);
+createSquare('yellow',0,100,100,100,'black')
+createSquare('blue',100,100,100,100,'black')
+createSquare('green',200,100,100,100,'black');
+createSquare('red',300,100,100,100,'black');
